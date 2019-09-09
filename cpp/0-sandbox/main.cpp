dcfb57143aefdabe5254a192e9a372b6e3534ceb
Sahil <sahilbondre@gmail.com>
2019-09-09 15:21:08 +0530

    cpp theory

--- cpp/0-sandbox/main.cpp
+++ cpp/0-sandbox/main.cpp
@@ -3,50 +3,16 @@
 
 using namespace std;
 
-void printVector(vector<int> vector)
+auto printVector(vector<int> vector)
 {
-  for (auto i = vector.begin(); i != vector.end(); ++i)
-    cout << *i << " ";
-  cout << endl;
-}
-
-void printVector(vector<vector<int>> vector)
-{
-  for (int i = 0; i < vector.size(); i++)
-    printVector(vector.at(i));
-}
-
-auto printVectorRangeBased(vector<int> vector)
-{
-  for (auto i : vector)
-    cout << i << " ";
+  for (int number : vector)
+    cout << number << " ";
   cout << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-  vector<int> vector1;
-  vector<int> vector2;
-  vector1.push_back(10);
-  vector1.push_back(20);
-
-  vector2.push_back(100);
-  vector2.push_back(200);
-
-  printVectorRangeBased(vector1);
-  printVector(vector2);
-
-  vector<vector<int>> vector_2d;
-
-  vector_2d.push_back(vector1);
-  vector_2d.push_back(vector1);
-
-  printVector(vector_2d);
-
-  vector1.at(0) = 1000;
-
-  printVector(vector_2d);
-
-  cout << *vector2.end() << endl;
+  vector<int> vec = {1, 2, 3, 4, 5};
+  printVector(vec);
   return 0;
 }
---
+++ notes/cpp/7-statement-and-operators.md
@@ -0,0 +1,24 @@
+# Statement and Operators
+
+### Type Conversion
+Methods:
+* C style casting: (type)value [Gives Runtime Errors]
+* function style casting: type(value) [Gives Runtime Errors]
+* static_cast: Prefer this over above two [Gives Complile-time Errors]
+* dynamic_cast: Used for polymorphism
+* const_cast: To toggle const
+* reinterpret_cast: To cast between any types of pointers
+
+syntax: ```static_cast<type>(variable)```
+
+> Note: static_cast, dynamic_cast, const_cast, reinterpret_cast are not functions but operators and thus are part of the c++ keywords
+
+> Note: Do not cast const variable to non const and then change it. The behavior is undefined in this case.
+```c++
+// Do NOT do this
+const int a = 5;
+int* b = const_cast<*int>(&a);
+*b = 6;
+```
+
+A good use case for const_cast is when some function requires const argument and you have a non const argument and vice-versa.
