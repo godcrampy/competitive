6988d73143267bb141b20de53fd0088d1c37deed
Sahil <sahilbondre@gmail.com>
2019-09-19 13:17:54 +0530

    spoj-250 23 ac-in-one

---
+++ problems/spoj-250/23-will-it-end/build.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  long long n;
+  std::cin >> n;
+  if ((n & (n - 1)) == 0)
+    // square number: it'll end
+    std::cout << "TAK" << std::endl;
+  else
+    std::cout << "NIE" << std::endl;
+  return 0;
+}
---
+++ problems/spoj-250/23-will-it-end/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "util.hpp"
+
+int main(int argc, char const *argv[])
+{
+  long long n;
+  std::cin >> n;
+  if (n & (n - 1) == 0)
+    // square number: it'll end
+    std::cout << "TAK" << std::endl;
+  else
+    std::cout << "NIE" << std::endl;
+  return 0;
+}
