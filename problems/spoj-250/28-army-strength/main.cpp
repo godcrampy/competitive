8ffcdcc5ecf9c4b18a90f205b474718255a0abd9
Sahil <sahilbondre@gmail.com>
2019-09-21 09:34:57 +0530

    spoj-250 28 ac-in-one

---
+++ problems/spoj-250/28-army-strength/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+  int t;
+  std::cin >> t;
+  for (int _ = 0; _ < t; _++)
+  {
+    std::getchar();
+    std::vector<int> godzilla_army, mech_army;
+    int godzilla_army_count, mech_army_count;
+    std::cin >> godzilla_army_count >> mech_army_count;
+    for (auto i = 0; i < godzilla_army_count; ++i)
+    {
+      int temp;
+      std::cin >> temp;
+      godzilla_army.push_back(temp);
+    }
+    for (auto i = 0; i < mech_army_count; ++i)
+    {
+      int temp;
+      std::cin >> temp;
+      mech_army.push_back(temp);
+    }
+    std::sort(godzilla_army.begin(), godzilla_army.end(), std::greater<int>());
+    std::sort(mech_army.begin(), mech_army.end(), std::greater<int>());
+    while (godzilla_army_count != 0 && mech_army_count != 0)
+    {
+      if (godzilla_army[godzilla_army_count - 1] < mech_army[mech_army_count - 1])
+      {
+        godzilla_army.pop_back();
+        --godzilla_army_count;
+      }
+      else
+      {
+        mech_army.pop_back();
+        --mech_army_count;
+      }
+    }
+    if (mech_army_count)
+      std::cout << "MechaGodzilla" << std::endl;
+    else
+      std::cout << "Godzilla" << std::endl;
+  }
+  return 0;
+}
