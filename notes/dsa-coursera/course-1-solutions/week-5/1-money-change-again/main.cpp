64bc6fe0b462a78d1cd708607e07903d550bd447
Sahil <sahilbondre@gmail.com>
2019-10-28 18:31:57 +0530

    week 5 problem 1 ac in 1

---
+++ notes/dsa-coursera/course-1-solutions/week-5/1-money-change-again/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+// available denominations  = 1, 3, 4
+
+int tri_min(int a, int b, int c)
+{
+  return min(a, min(b, c));
+}
+
+int money_change(int n)
+{
+  vector<int> table(n + 1, 0);
+  table[0] = 0;
+  for (auto i = 1; i <= n; ++i)
+  {
+    int a = table[i - 1];
+    int b = i - 3 >= 0 ? table[i - 3] : INT16_MAX;
+    int c = i - 4 >= 0 ? table[i - 4] : INT16_MAX;
+    table[i] = tri_min(a, b, c) + 1;
+  }
+  return table[n];
+}
+
+int main(int argc, char const *argv[])
+{
+  int n;
+  cin >> n;
+  cout << money_change(n) << endl;
+  return 0;
+}
