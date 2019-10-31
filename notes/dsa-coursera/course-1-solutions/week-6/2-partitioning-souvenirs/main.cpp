8d6566d4540210d6801552a537d64a76467a567e
Sahil <sahilbondre@gmail.com>
2019-11-01 00:07:33 +0530

    course 1 complete

---
+++ notes/dsa-coursera/course-1-solutions/week-6/2-partitioning-souvenirs/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool does_partition_exist(vector<int> weights)
+{
+  // assumption 1 indexed vector weights
+  int n = weights.size() - 1;
+  int w_sum = 0;
+  for (int weight : weights)
+    w_sum += weight;
+  if (w_sum % 3 != 0)
+    return false;
+  int w_each = w_sum / 3;
+
+  // can fit 1
+  vector<vector<int>> table_1(n + 1, vector<int>(w_each + 1, 0));
+  for (int i = 0; i <= n + 1; ++i)
+    table_1[i][0] = 1;
+
+  for (int i = 1; i <= n + 1; ++i)
+    for (int j = 1; j <= w_each + 1; ++j)
+    {
+      int a = j - weights[j] >= 0 ? table_1[i - 1][j - weights[j]] : 0;
+      int b = table_1[i - 1][j];
+      table_1[i][j] = a || b;
+    }
+
+  if (table_1[n][w_each] != 1)
+    return false;
+
+  
+}
---
+++ notes/dsa-coursera/course-1-solutions/week-6/3-maximum-value-of-expression/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+T minimum_of_five(T a, T b, T c, T d, T e)
+{
+  return min(a, min(b, min(c, min(d, e))));
+}
+
+template <class T>
+T maximum_of_five(T a, T b, T c, T d, T e)
+{
+  return max(a, max(b, max(c, max(d, e))));
+}
+
+template <class T>
+T operate(T a, char op, T b)
+{
+  switch (op)
+  {
+  case '+':
+    return a + b;
+  case '-':
+    return a - b;
+  case '*':
+    return a * b;
+  case '/':
+    return a / b;
+  default:
+    cout << "Error: invalid operator" << endl;
+  }
+  return a;
+}
+
+long long maximum_value(string &expression)
+{
+  int length = expression.length();
+  int n = (length + 1) / 2; // number of digits
+
+  // 1 index array
+  vector<int> numbers(n + 1, 0);
+  vector<char> operations(n, '.');
+
+  for (int i = 0; i < length; ++i)
+    if (i % 2 == 0)
+      // char at i is a digit
+      numbers[i / 2 + 1] = expression[i] - '0';
+    else
+      // char at i is an operation
+      operations[(i + 1) / 2] = expression[i];
+
+  vector<vector<long long>> max_table(n + 1, vector<long long>(n + 1, 0));
+  vector<vector<long long>> min_table(n + 1, vector<long long>(n + 1, 0));
+
+  for (auto i = 1; i <= n; ++i)
+  {
+    max_table[i][i] = numbers[i];
+    min_table[i][i] = numbers[i];
+  }
+
+  for (auto s = 1; s <= n - 1; ++s)
+    for (auto i = 1; i <= n - s; ++i)
+    {
+      auto j = i + s;
+      long long mini = __LONG_LONG_MAX__;
+      long long maxi = -__LONG_LONG_MAX__ - 1;
+      for (auto k = i; k <= j - 1; ++k)
+      {
+        long long m_l = min_table[i][k];
+        long long M_l = max_table[i][k];
+        long long m_r = min_table[k + 1][j];
+        long long M_r = max_table[k + 1][j];
+        char op = operations[k];
+        mini = minimum_of_five(mini, operate(m_l, op, m_r), operate(m_l, op, M_r), operate(M_l, op, m_r), operate(M_l, op, M_r));
+        maxi = maximum_of_five(maxi, operate(m_l, op, m_r), operate(m_l, op, M_r), operate(M_l, op, m_r), operate(M_l, op, M_r));
+      }
+      max_table[i][j] = maxi;
+      min_table[i][j] = mini;
+    }
+  return max_table[1][n];
+}
+
+int main(int argc, char const *argv[])
+{
+  string input;
+  cin >> input;
+  cout << maximum_value(input) << endl;
+  return 0;
+}
