68491e23827763c28444ae1f419b6c6e366d063d
Sahil <sahilbondre@gmail.com>
2020-01-16 14:36:32 +0530

    Add ctci 1.9

---
+++ problems/ctci/1-9/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_substring(string s, string a) {
+  return s.find(a) != s.npos;
+}
+
+bool are_strings_rotated(string s1, string s2) {
+  string s1s1 = s1 + s1;
+  if (s1.length() != s2.length()) {
+    return false;
+  }
+  return is_substring(s1s1, s2);
+}
+
+int main() {
+  string s = "hacktoberfest", str = "aesthacktober";
+  cout << are_strings_rotated(s, str) << endl;
+  return 0;
+}
