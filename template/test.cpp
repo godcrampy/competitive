3970f1a45cf787105dd8d4fbd0c1da521115f219
Sahil <sahilbondre@gmail.com>
2020-06-17 22:07:22 +0530

    replace test template

--- template/test.cpp
+++ template/test.cpp
@@ -1,5 +1,33 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char const *argv[]) {
+using namespace std;
+
+int randomNumber(int l, int r) {
+  // * Returns a random integer from [l, r]
+  return l + (std::rand() % (r - l + 1));
+}
+
+template <typename T>
+void printVector(const vector<T>& v, string msg) {
+  cout << msg;
+  for (auto n : v) {
+    cout << n << " ";
+  }
+  cout << endl;
+}
+
+void smokeTest() {
+}
+
+int main(int argc, char const* argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  srand(time(0));
+
+  smokeTest();
   return 0;
 }
