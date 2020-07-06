e128dcc9e28738fdd2af3c6c589d44050f49b521
Sahil <sahilbondre@gmail.com>
2020-07-06 17:45:01 +0530

    add gfg 17

--- problems/gfg/17-stock-buy-and-sell/main.cpp
+++ problems/gfg/17-stock-buy-and-sell/main.cpp
@@ -40,3 +40,14 @@ void solve() {
     cout << '\n';
   }
 }
+
+int main(int argc, char const* argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; ++i) {
+    solve();
+  }
+  return 0;
+}
