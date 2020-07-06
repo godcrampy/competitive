4499f926cdecf3914b1fbb5417421c268f9bd434
Sahil <sahilbondre@gmail.com>
2020-07-06 17:29:24 +0530

    add gfg 17

---
+++ problems/gfg/17-stock-buy-and-sell/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+
+  long profit = 0;
+  vector<pair<int, int>> res;
+  int i = 0;
+  while (i < n) {
+    int j = i + 1;
+    int prev = v[i];
+    while (j < n && v[j] >= prev) {
+      prev = v[j];
+      ++j;
+    }
+    if (v[j - 1] - v[i] > 0) {
+      res.push_back(make_pair(i, j - 1));
+      i = j;
+    } else {
+      ++i;
+    }
+  }
+  if (res.size() == 0) {
+    cout << "No Profit" << endl;
+  } else {
+    for (auto p : res) {
+      cout << "(" << p.first << " " << p.second << ")"
+           << " ";
+    }
+    cout << '\n';
+  }
+}
