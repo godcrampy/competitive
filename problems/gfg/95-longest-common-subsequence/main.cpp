1a5283f9d12b3fa1b0833bcf840c61bfa3ffdf0e
Sahil <sahilbondre@gmail.com>
2020-08-03 19:33:30 +0530

    add gfg 95

---
+++ problems/gfg/95-longest-common-subsequence/main.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+  int m, n;
+  cin >> m >> n;
+  string a, b;
+  cin >> a >> b;
+
+  vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+  for (int i = 0; i <= m; ++i) {
+    for (int j = 0; j <= n; ++j) {
+      if (i == 0 || j == 0) {
+        dp[i][j] = 0;
+      } else if (a[i - 1] == b[j - 1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  cout << dp[m][n] << endl;
+}
+
+int main(int argc, char const* argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; ++i) {
+    solve();
+  }
+  return 0;
+}
