6bd77026ac4969c6b995a5b87e03e3db9fc8628c
Sahil Bondre <sahilbondre@gmail.com>
2020-11-16 22:03:50 +0530

    update gfg-23 add implementation

--- problems/gfg/23-string-permutations/main.cpp
+++ problems/gfg/23-string-permutations/main.cpp
@@ -2,6 +2,36 @@
 #include <iostream>
 using namespace std;
 
+set<string> getPermutations(string &s) {
+  if (s.length() == 1) {
+    return {s};
+  }
+
+  string f = s.substr(0, 1);
+  string rest = s.substr(1);
+  auto perms = getPermutations(rest);
+  set<string> res;
+
+  for (string str : perms) {
+    for (int i = 0; i <= str.length(); ++i) {
+      string start = str.substr(0, i);
+      string end = str.substr(i);
+      res.insert(start + f + end);
+    }
+  }
+  return res;
+}
+
+void solve2() {
+  string s;
+  cin >> s;
+  auto perms = getPermutations(s);
+  for (string str : perms) {
+    cout << str << " ";
+  }
+  cout << "\n";
+}
+
 void solve() {
   string s;
   cin >> s;
